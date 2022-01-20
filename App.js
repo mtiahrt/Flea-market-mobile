817b77830a71be41e61f51ac9e877522a6e2f81e
mtiahrt <mark.tiahrt@outlook.com>
2022-01-20 08:27:45 -0700

    add splash screen and icon

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+import { Asset } from 'expo-asset';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import FleaMarketStackNav from './navigation/FleaMarketNavigator';
 import { CartProvider } from './contexts/CartContext';
@@ -12,6 +15,26 @@ const theme = {
 };
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const cacheResourcesAsync = async () => {
+    const images = [require('./assets/icon.png')];
+
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+    return Promise.all(cacheImages);
+  };
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={cacheResourcesAsync}
+        onFinish={setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <PaperProvider theme={theme}>
       <CartProvider>
